6bc47087804e10dc369060f8631e824383decec0
just <adm@biwt.ru>
2026-02-13 13:14:25 +0300

    Fix component router service for Joomla

--- component/com_glavpro_crm/administrator/components/com_glavpro_crm/src/Extension/GlavproCrmComponent.php
+++ component/com_glavpro_crm/administrator/components/com_glavpro_crm/src/Extension/GlavproCrmComponent.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Glavpro\Component\GlavproCrm\Administrator\Extension;
 
+use Joomla\CMS\Component\Router\RouterServiceInterface;
+use Joomla\CMS\Component\Router\RouterServiceTrait;
 use Joomla\CMS\Extension\MVCComponent;
 
-final class GlavproCrmComponent extends MVCComponent
+final class GlavproCrmComponent extends MVCComponent implements RouterServiceInterface
 {
+    use RouterServiceTrait;
 }
