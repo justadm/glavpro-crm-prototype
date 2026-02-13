b578cf91e465074e4e2e71bce7cf06f375e6f692
just <adm@biwt.ru>
2026-02-13 13:49:59 +0300

    Add site dispatcher for frontend

---
+++ component/com_glavpro_crm/components/com_glavpro_crm/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Glavpro\Component\GlavproCrm\Site\Dispatcher;
+
+use Joomla\CMS\Dispatcher\ComponentDispatcher;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+final class Dispatcher extends ComponentDispatcher
+{
+    // Default dispatcher behaviour is enough for this prototype.
+}
+
